016eb0d351620d1ab4f1ff8d7dc6480edb1c7d99
pyzlnar@remote-ubuntu <pyzlnar@gmail.com>
2021-12-11 17:39:28 +0000

    :zap: Added extra tests to TicTacToe
    Namely a stalemate test and some other edgecases.
    More to be added later... maybe

--- test/tic_tac_toe_test.exs
+++ test/tic_tac_toe_test.exs
@@ -17,8 +17,38 @@ defmodule TicTacToeTest do
       result = game |> TicTacToe.play({0,2})
       assert {:finished, %Game{} = final} = result
 
+      assert final.state  == :finished
+      assert final.winner == 1
+    end
+
+    test "game with stalemate" do
+      game = TicTacToe.new_game
+
+      result = game |> TicTacToe.play({0,0})
+      assert {:ok, %Game{}} = result
+
+      game |> TicTacToe.play({1,1})
+      game |> TicTacToe.play({0,1})
+      game |> TicTacToe.play({0,2})
+      game |> TicTacToe.play({2,0})
+      game |> TicTacToe.play({1,0})
+      game |> TicTacToe.play({1,2})
+      game |> TicTacToe.play({2,1})
+
+      result = game |> TicTacToe.play({2,2})
+      assert {:finished, %Game{} = final} = result
+
       assert final.state == :finished
-      assert is_integer(final.winner)
+      assert is_nil(final.winner)
+    end
+  end
+
+  describe "other edgecases and errors" do
+    test "returns error if trying to start an already existing game" do
+      id = make_ref()
+      TicTacToe.new_game(id)
+
+      assert {:error, {:already_started, _}} = TicTacToe.new_game(id)
     end
   end
 end
